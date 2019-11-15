01122aa2f4316902a6c3107d9040accd030c98ed
loboswork <30801658+loboswork@users.noreply.github.com>
2019-11-15 23:46:59 +0900

    Add files via upload

---
+++ Aura01_src/aura/SimilarProperty/SimilarPropertyController.js
@@ -0,0 +1,40 @@
+({
+    navToRecord : function(component, event, helper) {
+        var navEvt = $A.get("e.force:navigateToSObject");
+        navEvt.setParams({
+            "recordId" : component.get("v.property.Id")
+        });
+        navEvt.fire();
+
+    },
+    editRecord : function(component, event, helper) {
+        // var editRecordEvent = $A.get("e.force:editRecord");
+        // editRecordEvent.setParams({
+        //     "recordId": component.get("v.property.Id")
+        // });
+        // editRecordEvent.fire();
+// debugger
+        helper.showHide(component);
+    },
+    handleSuccess : function(component, event, helper) {
+console.log("handleSuccess");
+// debugger
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            "title": "Success!",
+            "message": "The property's info has been updated.",
+            "type": "success"
+        });
+        toastEvent.fire();
+
+        var recUpdate = $A.get("e.c:recordUpdated");
+        recUpdate.fire();
+
+        helper.showHide(component);
+    },
+    handleCancel : function(component, event, helper) {
+        helper.showHide(component);
+        event.preventDefault();
+    }
+    
+})
